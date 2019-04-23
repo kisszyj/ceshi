19e5357089c876ffa7008c2592d9c3a1177f7525
[yang0120] <[kissyouzyjni@live.com]>
2019-04-23 15:00:57 +0800

    删除

--- config/prod.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const path = require('path');
-require('dotenv').config({ path: path.resolve(process.cwd(), './.env.production') });
-const merge = require('webpack-merge')
-module.exports = {
-  NODE_ENV: '"production"',
-  API_HOST: '"'+process.env.APIHOST+'"',
-  CMS_HOST: '"'+process.env.CMS_HOST+'"',
-}
